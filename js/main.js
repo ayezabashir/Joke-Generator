7bda73b2b91558f7c438c8822a4193497e5e1cda
Ayeza <ayezabashir442@gmail.com>
2023-04-02 01:35:19 +0500

    cracking jokes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 console.log("Random Joke Generator");
+
+const jokeBox = document.querySelector(".actualJoke");
+const btn = document.querySelector(".btn");
+const jokeUrl = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,sexist,explicit&type=single";
+
+function crackJoke() {
+    jokeBox.classList.remove("effect");
+    fetch(jokeUrl)
+        .then(data => data.json())
+        .then(item => {
+            jokeBox.innerText = `${item.joke}`;
+            jokeBox.classList.add("effect");
+        })
+}
+
+btn.addEventListener("click", function () {
+    crackJoke();
+});
